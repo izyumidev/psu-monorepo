0e719c158daf72014a5938879bb481ca76e12e01
Yumi Izumi <me@yumiizumi.com>
2024-01-24 15:35:06 -0500

    allow text input instead of ascii

--- ascii-xor/src/main.rs
+++ ascii-xor/src/main.rs
@@ -2,19 +2,13 @@ fn main() {
     let mut line1 = String::new();
     let mut line2 = String::new();
 
+    println!("Enter the message: ");
     std::io::stdin().read_line(&mut line1).unwrap();
+    println!("Enter the key: ");
     std::io::stdin().read_line(&mut line2).unwrap();
 
-    let mes_ascii = line1
-        .trim()
-        .split_whitespace()
-        .map(|x| x.parse::<u8>().unwrap())
-        .collect::<Vec<u8>>();
-    let key_ascii = line2
-        .trim()
-        .split_whitespace()
-        .map(|x| x.parse::<u8>().unwrap())
-        .collect::<Vec<u8>>();
+    let mes_ascii = line1.trim().chars().map(|x| x as u8).collect::<Vec<u8>>();
+    let key_ascii = line2.trim().chars().map(|x| x as u8).collect::<Vec<u8>>();
 
     for letter in mes_ascii.iter().zip(key_ascii.iter()) {
         print!("{} ", letter.0 ^ letter.1);
